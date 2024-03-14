faa41c058bd8d9124f5431c73c537b8ffdb0d734
khlood-ebada <khloodebada97@gmail.com>
2024-03-14 13:40:59 -0700

    a

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print all hidden diectories"""
-    from hidden_4 impory *
+    import hidden_4
 
-    for i in dir(hidden_4) :
+    for i in dir(hidden_4):
         if i[:2] != "__":
             print(i)
