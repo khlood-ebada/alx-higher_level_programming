3bdbc34a6f51838897a4b3a5671761d36b2feb4d
khlood-ebada <khloodebada97@gmail.com>
2024-03-14 14:13:50 -0700

    a

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,9 @@
-def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    def magic_calculation(a, b):
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
