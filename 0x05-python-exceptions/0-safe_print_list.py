56edc1430dee0332f4b2f653fcb0c497c8cd1cd8
khlood-ebada <khloodebada97@gmail.com>
2024-04-02 13:52:26 -0700

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except:
+        except IndexError:
             break
-    else:
+        else:
             count += 1
 
     print()
