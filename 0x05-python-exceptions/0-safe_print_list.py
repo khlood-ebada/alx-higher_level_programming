c7a15701eae376992516ac8735cee8416822414c
khlood-ebada <khloodebada97@gmail.com>
2024-04-02 12:22:23 -0700

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
 
-    while i is not x:
+    for i in range(x):
         try:
             print(my_list[i], end='')
+        except:
+            break
+        else:
             count += 1
-        except IndexError:
-            None
 
     print()
     return (count)
