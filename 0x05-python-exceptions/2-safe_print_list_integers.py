5ad23a955f9d963680f587e0620bd69a7f65c8a6
khlood-ebada <khloodebada97@gmail.com>
2024-04-02 13:58:11 -0700

    a

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass
         i += 1
-    print()    
-    return (count)
+
+print()    
+return (count)
