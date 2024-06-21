57528869ab7d186fc7ebd7d2dbbcbc2cef4d1981
khlood-ebada <khloodebada97@gmail.com>
2024-06-21 06:35:14 -0700

    a

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-// script that prints “JavaScript is amazing”
-const myvar = 'JavaScript is amazing';
-console.log(myvar);
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
